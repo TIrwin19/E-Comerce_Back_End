51cd8ffdd3b9f20ed93d12940fe1373db1a942d4
Blackbox19 <trevorirwin819@gmail.com>
2024-04-27 22:18:09 -0400

    still bugs in fireign keys

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,12 +3,12 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try{
-    const allTags = Tag.findAll({
-      include: Product
+    const allTags = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag }]
     })
 
     res.json(allTags)
@@ -18,12 +18,12 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   const id = req.params.id
   try{
-    const tag = Tag.findByPk(id, {
+    const tag = await Tag.findByPk(id, {
       include: Product
     })
 
@@ -38,7 +38,7 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try{
     const newTag = req.body
@@ -47,7 +47,7 @@ router.post('/', (req, res) => {
       return res.json({message: 'No Tag entered'})
     }
     
-    const addTag = Tag.create(newTag)
+    const addTag = await Tag.create(newTag)
     res.json(addTag)
 
   } catch(err) {
@@ -55,18 +55,18 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   const id = req.params.id
   try{
     const newTagName = req.body.tag_name
 
-    const tag = Tag.findByPk(id)
+    const tag = await Tag.findByPk(id)
     if(!tag) {
       return res.json({message: 'Tag not found'})
     }
 
-    const updateTag = Tag.update({tag_name: newTagName}, {
+    const updateTag = await Tag.update({tag_name: newTagName}, {
       where: {
         tag_id: id
       }
@@ -78,11 +78,11 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   const id = req.params.id
   try {
-    const tag = Tag.findByPk(id)
+    const tag = await Tag.findByPk(id)
 
     if (tag) {
       tag.destroy()
